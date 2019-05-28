5b05f5f656afdfd14c8ca39ad0c7f6fb03ecdea8
Long Lee <lethlong79@gmail.com>
2019-05-28 09:41:24 +0700

    Delete some test files and icons

--- test-TX-Address-MijinTestnet.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -i
-source /home/jd/app/code/ledgerHQ/BOLOS-DEVENV/ledger/bin/activate
-python test-TX-Address.py --ed25519 --path "44'/43'/144'/1'/1'"
-#python test-TX-Address.py --ed25519 --path "44'/43'/104'/1'/9'"
